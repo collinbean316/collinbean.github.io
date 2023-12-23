207a3eac750625be029fd52ec1f9234241c203b4
Collin Bean <collinbean316@users.noreply.github.com>
2023-12-22 19:51:33 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 let mybutton = document.querySelector("button");
 
-const arr = [["h","a","y","a","o","","s","a","s","h"],
-["o","r","a","l","b","","a","l","o","e"],
-["t","e","s","l","a","","l","a","d","y"],
-["","","","e","m","u","s","","",""],
-["","m","i","y","a","z","a","k","i",""],
-["l","e","s","s","s","o","","a","n","t"],
-["i","l","l","","","","s","i","t","e"],
-["e","t","a","","i","r","i","s","e","s"],
-["t","e","n","","t","a","l","e","n","t"],
-["o","d","d","","s","w","o","r","d","s"]]
+const arr = [["n","b","a","","","l","a","w","s","","d","r","e","a","d"],
+["m","o","g","a","d","i","s","h","u","","o","c","e","a","n"],
+["r","a","n","s","o","m","m","e","d","","g","a","l","a","s"],
+["","","","k","h","a","r","t","o","u","m","","","",""],
+["w","i","k","i","","","","","","n","a","","g","a","p"],
+["t","o","o","n","","a","r","k","","","","","a","g","e"],
+["o","u","a","g","a","d","o","u","g","o","u","","s","o","n"],
+["","","","","m","a","n","d","a","l","a","","","",""],
+["p","a","d","","b","r","a","z","z","a","v","i","l","l","e"],
+["i","c","e","","","","","u","a","w","","m","o","a","n"],
+["n","e","w","","s","t","","","","","","a","c","m","e"],
+["","","","","c","a","p","i","t","a","l","s","","",""],
+["s","c","u","b","a","","u","n","a","l","l","o","y","e","d"],
+["c","o","r","e","r","","b","u","j","u","m","b","u","r","a"],
+["h","o","n","e","y","","s","t","o","m","","","m","r","s"],
+]
 
 
 function checkAnswers () {
     let errors = 0
-    for (var  row = 0; row < 10; row++) {
-        for (var col = 0; col < 10; col++) {
-            var ident=((row+1).toString())+((col+1).toString())
+    for (var  row = 0; row < 15; row++) {
+        for (var col = 0; col < 15; col++) {
+            var ident=((row+1).toString())+"-"+((col+1).toString())
             const entry = document.getElementById(ident).value
             const ans = arr[row][col]
             if (ans != entry) {
